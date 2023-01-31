553b231eac51eb6e5378f9a31be37dcb6bc5812e
stoXmod <nishsrasenadheera2@gmail.com>
2023-01-31 19:50:44 +0530

    change mongodbURI

--- app.js
+++ app.js
@@ -1,57 +1,62 @@
-var createError = require('http-errors');
-var cors = require('cors');
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
-var mongoose = require('mongoose');
-var flash = require('connect-flash');
-var env = require('./env');
+require("dotenv").config();
+var createError = require("http-errors");
+var cors = require("cors");
+var express = require("express");
+var session = require("express-session");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var sassMiddleware = require("node-sass-middleware");
+var mongoose = require("mongoose");
+var flash = require("connect-flash");
+var env = require("./env");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-    secret: env.salt,
-    cookie: { maxAge: 60000 }
-}));
+app.use(
+    session({
+        secret: env.salt,
+        cookie: { maxAge: 60000 },
+    })
+);
 app.use(cookieParser());
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: false, // true = .sass and false = .scss
-    sourceMap: true
-}));
+app.use(
+    sassMiddleware({
+        src: path.join(__dirname, "public"),
+        dest: path.join(__dirname, "public"),
+        indentedSyntax: false, // true = .sass and false = .scss
+        sourceMap: true,
+    })
+);
 app.use(flash());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Database
 if (env.isProduction) {
     mongoose.connect(process.env.MONGODB_URI);
 } else {
-    mongoose.connect('mongodb://localhost/oauth', { useNewUrlParser: true });
-    mongoose.set('debug', true);
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+    mongoose.set("debug", true);
 }
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB error: '));
-db.once('open', console.log.bind(console, 'MongoDB connection successful'));
+db.on("error", console.error.bind(console, "MongoDB error: "));
+db.once("open", console.log.bind(console, "MongoDB connection successful"));
 
-require('./models/user');
-require('./models/oauth');
+require("./models/user");
+require("./models/oauth");
 
 // Routes
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -66,7 +71,7 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
 });
 
 module.exports = app;
